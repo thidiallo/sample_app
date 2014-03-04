f8ef5e97d7a55ed140160d4036c42ddd0eb543a0
thidiallo <thidiallo40@gmail.com>
2014-03-04 16:25:25 +0100

    Fin du layout et des routages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+class UsersController < ApplicationController
+
+  def new
+    @titre = "Inscription"
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe UsersController do
+  render_views
+
+  describe "GET 'new'" do
+    it "devrait réussir" do
+      get 'new'
+      response.should be_success
+    end
+
+    it "devrait avoir le titre adéquat" do
+      get 'new'
+      response.should have_selector("title", :content => "Inscription")
+    end
+  end
+end
+
